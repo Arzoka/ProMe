8a95f5974fde501ea42e5838b3f12a73f6f777bc
Arzoka <lockxm@gmail.com>
2025-02-07 14:41:59 +0100

    fixed imports and file structure

--- client/src/pages/dashboard/lilguys/create/index.tsx
+++ client/src/pages/dashboard/lilguys/create/index.tsx
@@ -1,22 +1,21 @@
 import styles from './index.module.scss';
 import { Fragment } from 'react';
-import SideBar from './sidebar/index.tsx';
-import LilGuyMaker from './lilGuyMaker/index.tsx';
+import LilGuyMaker from '@/pages/dashboard/lilguys/create/sub-components/lil-guy-maker';
+import SideBar from '@/pages/dashboard/lilguys/create/sub-components/sidebar';
 
 const Create = () => {
 
 	return (
 		<Fragment>
 			<main>
-				<section className={styles.sideBar}>
+				<section className={ styles.sideBar }>
 					<SideBar />
 				</section>
 				<section className={ styles.lilGuy }>
 					<LilGuyMaker />
 				</section>
 			</main>
-		</Fragment>
-	);
+		</Fragment> );
 };
 
 export default Create;
