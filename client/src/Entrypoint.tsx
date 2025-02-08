13d6b578479675c5bcd3b0e6458290de5ca57550
Arzoka <lockxm@gmail.com>
2025-02-08 13:37:29 +0100

    toast container

--- client/src/Entrypoint.tsx
+++ client/src/Entrypoint.tsx
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
 import router from '@/routes';
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 const Entrypoint = () => {
 
 	return (
 		<Fragment>
 			<RouterProvider router={ router } />
+			<ToastContainer />
 		</Fragment> );
 };
 
